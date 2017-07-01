3557c2c19d7f3e886058703976ae3ac1a338d18f
chipmunk-sm <dannico@linuxmail.org>
2017-07-02 02:50:26 +0300

    update debian build

--- prepare.sh
+++ prepare.sh
@@ -4,13 +4,25 @@ set -e
 
 RFOLDER="`pwd`"
 
-mv ${RFOLDER}/debian/changelog ${RFOLDER}/debian/changelog.template
-DISTRIB=$(lsb_release -c -s)
-sed "s/unstable/${DISTRIB}/g" ${RFOLDER}/debian/changelog.template > ${RFOLDER}/debian/changelog	
+echo "***Prepare test build***"
 
-qmake ./tcpview.pro -r -spec linux-g++-64 MOC_DIR=build/release RCC_DIR=build/release UI_DIR=build/release OBJECTS_DIR=build/release
+mv $RFOLDER/debian/changelog $RFOLDER/debian/changelog.template
+DISTRIB=$(lsb_release -c -s)
+sed "s/unstable/$DISTRIB/g" $RFOLDER/debian/changelog.template > $RFOLDER/debian/changelog	
+
+if [ ! $ARCHITECTURE ]; then
+		ARCHITECTURE=64
+fi
+
+if [ ! $TMPBUILDDIR ]; then
+		TMPBUILDDIR=build/release
+fi
+
+qmake ./tcpview.pro -r -spec linux-g++-$ARCHITECTURE MOC_DIR=$TMPBUILDDIR RCC_DIR=$TMPBUILDDIR UI_DIR=$TMPBUILDDIR OBJECTS_DIR=$TMPBUILDDIR
 # make
 
-echo "DISTRIB=${DISTRIB}"
-echo "RFOLDER=${RFOLDER}"
+echo ">>>ARCHITECTURE\t=$ARCHITECTURE"
+echo ">>>TMPBUILDDIR\t=$TMPBUILDDIR"
+echo ">>>DISTRIB\t=$DISTRIB"
+echo ">>>RFOLDER\t=$RFOLDER"
 
