48389cb03dda2a0b1d7a1ff1bff93e276f90612b
chipmunk-sm <dannico@linuxmail.org>
2019-02-01 02:07:20 +0200

    merge to master (Add travis-ci, Build Status. Update info, copyright)

--- translations/language_en.ts
+++ translations/language_en.ts
@@ -4,14 +4,9 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.ui" line="14"/>
-        <source>TcpView</source>
-        <translation>TcpView</translation>
-    </message>
-    <message>
         <location filename="../mainwindow.ui" line="36"/>
         <source>Filter</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="49"/>
@@ -25,85 +20,85 @@ Word\b     - end with Word
 
 Example. Only UDP and TCP (not TCP6 ot UDP6)
 tcp\b|udp\b</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="92"/>
         <source>Stops loading records</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="95"/>
         <source>Pause</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="124"/>
         <source>Do not cleanup deleted records</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="127"/>
         <source>Record</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="143"/>
         <source>Copy to Clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="146"/>
         <source>Copy</source>
         <oldsource>Colpy</oldsource>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="159"/>
         <source>Save to XML or CVS</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="162"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="175"/>
         <source>Display program names (Password required)</source>
         <oldsource>Password required</oldsource>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="178"/>
         <source>Names</source>
         <oldsource>Display names</oldsource>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="191"/>
         <source>WHOIS - Lookup service to search for the domain name registration information</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="194"/>
         <source>whois</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="207"/>
         <source>Zoom</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="493"/>
+        <location filename="../mainwindow.cpp" line="520"/>
         <source>  %1 / %2  </source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="496"/>
+        <location filename="../mainwindow.cpp" line="523"/>
         <source> Visible %1 / Total %2 </source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -115,238 +110,251 @@ tcp\b|udp\b</source>
         <translation>English.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="389"/>
+        <location filename="../mainwindow.cpp" line="416"/>
         <source>Cannot start module, because 
 %1
  is missing or execution access is denied.</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="392"/>
-        <location filename="../mainwindow.cpp" line="422"/>
-        <location filename="../mainwindow.cpp" line="431"/>
+        <location filename="../mainwindow.cpp" line="419"/>
+        <location filename="../mainwindow.cpp" line="449"/>
+        <location filename="../mainwindow.cpp" line="458"/>
         <source>Run as root</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="422"/>
+        <location filename="../mainwindow.cpp" line="449"/>
         <source>Failed on start
 </source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="431"/>
+        <location filename="../mainwindow.cpp" line="458"/>
         <source>Failed on &apos;init&apos; client module</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="143"/>
+        <location filename="../mainwindow.cpp" line="145"/>
         <source>Failed on showEvent</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="335"/>
+        <location filename="../mainwindow.cpp" line="285"/>
+        <source>Command &apos;whois&apos; not found,
+but can be installed with:
+sudo apt install whois</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="291"/>
+        <source>Failed run &apos;whois&apos;:
+</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="362"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="340"/>
+        <location filename="../mainwindow.cpp" line="367"/>
         <source>Copy row</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="46"/>
         <source>Protocol</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="46"/>
         <location filename="../source/cconnectionstree.cpp" line="65"/>
         <source>tcp6</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="47"/>
         <source>Local Host</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="48"/>
         <source>Local Address</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="48"/>
         <location filename="../source/cconnectionstree.cpp" line="52"/>
         <source>255.255.255.255</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="49"/>
         <source>Local Port</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="49"/>
         <location filename="../source/cconnectionstree.cpp" line="53"/>
         <source>65535</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="50"/>
         <source>State</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="50"/>
         <source>ESTABLISHED</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="51"/>
         <source>Remote Host</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="52"/>
         <source>Remote Address</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="53"/>
         <source>Remote Port</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="54"/>
         <source>User</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="54"/>
         <location filename="../source/cconnectionstree.cpp" line="55"/>
         <source>0000000</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="55"/>
         <source>Inode</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="56"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="56"/>
         <source>2017-01-01 00:00:00</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="57"/>
         <source>Command</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="58"/>
         <source>RowId</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="63"/>
         <source>tcp</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="64"/>
         <source>udp</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="66"/>
         <source>udp6</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="67"/>
         <source>raw</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="68"/>
         <source>raw6</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="74"/>
         <source>established</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="75"/>
         <source>syn_sent</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="76"/>
         <source>syn_recv</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="77"/>
         <source>fin_wait1</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="78"/>
         <source>fin_wait2</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="79"/>
         <source>time_wait</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="80"/>
         <source>unconnected</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="81"/>
         <source>close_wait</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="82"/>
         <source>last_ack</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="83"/>
         <source>listen</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="84"/>
         <source>closing</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="85"/>
         <source>closed</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="86"/>
         <source>removed</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="242"/>
         <source>Save as ...</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cconnectionstree.cpp" line="281"/>
@@ -354,18 +362,19 @@ tcp\b|udp\b</source>
         <location filename="../source/cfilesavewrapper.cpp" line="44"/>
         <location filename="../source/cfilesavewrapper.cpp" line="84"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
+        <location filename="../mainwindow.cpp" line="278"/>
         <location filename="../source/cconnectionstree.cpp" line="285"/>
         <source>Unexpected exception</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../source/cfilesavewrapper.h" line="27"/>
         <source>Error: Cannot write file
 </source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 </TS>
